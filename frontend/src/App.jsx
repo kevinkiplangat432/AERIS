4e84348cf01a1b257f579f93ecc85b7311625dac
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-11-07 17:58:33 +0300

    the build commences here

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,12 @@
 import React, {useState, useEffect} from "react"
 import Header from "./components/Header";
-import AerisTest from "./components/Aeristest";
 
 function App(){
-  const [message, setmessage] = useState("loading...")
-
-  useEffect(()=>{
-    fetch("http://127.0.0.1:5000/api/test")
-    .then(res => res.json())
-    .then(data => setmessage(data.message))
-  },[]);
-
+  
   return(
     <>
     <Header/>
-    <h1>{message}</h1>
-    <AerisTest/>
+    
     </>
   )
 };
