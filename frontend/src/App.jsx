b6c533369d55cdd9eebfb2c1f999cf6881a955b2
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-11-07 10:20:21 +0300

    modified file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import React, {useState, useEffect} from "react"
 
 function App(){
-  const[message, setmessage]= useState("loading....")
+  const [message, setmessage] = useState("loading...")
 
-  useEffect(()=> {
-    fetch( "http://127.0.0.1:5000/api/test")
+  useEffect(()=>{
+    fetch("http://127.0.0.1:5000/api/test")
     .then(res => res.json())
-    .then(data=> setmessage(data.message));
+    .then(data => setmessage(data.message))
   },[]);
 
-  return <h1>{message}</h1>
-}
+  return(
+    <h1>{message}</h1>
+  )
+};
 
 export default App;
